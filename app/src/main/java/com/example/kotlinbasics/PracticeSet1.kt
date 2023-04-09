8fa36ac94b7feb6639e3d3326d592edca1119502
dipeshgupta <dipeshguptadeveloper@gmail.com>
2023-04-09 17:41:36 +0530

    Palindrome, largest 3, leap year, table print of any number & average of any number functionality added

--- app/src/main/java/com/example/kotlinbasics/PracticeSet1.kt
+++ app/src/main/java/com/example/kotlinbasics/PracticeSet1.kt
@@ -1,9 +1,14 @@
 package com.example.kotlinbasics
 
+import java.util.Scanner
+
 fun main() {
     val number = listOf(10, 20, 30, 40, 20)
     val largestThree = number.sortedDescending().take(3)
     println(largestThree)
+
+    val average = number.average()
+    println(average)
 
 
     val str1 = "level"
@@ -11,7 +16,34 @@ fun main() {
     println(isPalindrome(str1))
     println(isPalindrome(str2))
 
+
+    val year = 2022
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                println("$year is a leap year.")
+            } else {
+                println("$year is not a leap year.")
+            }
+        } else {
+            println("$year is a leap year.")
+        }
+    } else {
+        println("$year is not a leap year.")
+    }
+
+    println("Enter any number to print its table : ")
+    val tableNumber = Scanner(System.`in`).nextInt()
+
+    for (i in 1..10) {
+        val result = tableNumber * i
+        println("$tableNumber x $i = $result")
+    }
 }
+
+
+// Palindrome function
 
 fun isPalindrome(str: String): Boolean {
     val length = str.length
@@ -22,3 +54,4 @@ fun isPalindrome(str: String): Boolean {
     }
     return true
 }
+
